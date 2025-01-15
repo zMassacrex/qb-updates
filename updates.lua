387bac38335ea93ead180196ea9674c631208dab
zMaSs4cR3x <106979384+zMassacrex@users.noreply.github.com>
2025-01-15 14:28:07 -0500

    Update updates.lua

--- updates.lua
+++ updates.lua
@@ -1,5 +1,5 @@
 {
-    "version": "1.0.1",
+    "version": "1.0.2",
     "changeLogs": "Revisa en nuestro discord https://discord.gg/rCU98CHnXp por nuevos updates en el canal QBCore-Update",
-    "releaseDate": "1/6/25"
+    "releaseDate": "1/15/25"
 }
